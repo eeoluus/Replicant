3197d2d2c4046b94b85b9d7bec27de47e4579427
Eero Uusaro <eerouusaro@gmail.com>
2022-01-07 16:01:51 +0200

    now in multicolor! also scrollbar

--- Replicant.py
+++ Replicant.py
@@ -1,12 +1,12 @@
 import sys
-import tkinter as tk
+from tkinter import Tk, Entry, END, BOTH
+from tkinter.scrolledtext import ScrolledText
+from os import listdir
+from os.path import splitext
+from importlib.util import find_spec, module_from_spec
 
-from os import getcwd, listdir
-from os.path import isfile, join
-from importlib.util import module_from_spec, find_spec
 
-
-class StandardOutput:
+class StandardStream:
     """Context manager for redirection of output and error streams."""
     
     def __init__(self, newOutput):
@@ -26,108 +26,89 @@ class StandardOutput:
 def moduleEntry(event):
     """Cache the specs of a chosen module."""
     userInput = event.widget.get()
-    event.widget.delete(0, tk.END)
-    print(userInput)
+    event.widget.delete(0, END)
+    log.write(userInput, 'green')
         
-    if isStrat(userInput, stratNames):
+    if userInput in pipelines:
         cache['handlers'] = (
             spec 
             for spec in inspect(
-                'strats', 
+                'pipelines', 
                 userInput
                 )
             )
     else:
-        print(msgs['invalid'])
-
-def isStrat(userInput, strats):
-    """Check if user input exists in strategies."""
-    if userInput in strats:
-        return True
-    return False
+        log.write(msgs['invalid'], 'green')
         
 def inspect(package, module):
     """Pull out and print module source code, ask for user approval and change the entry callback.
     
     Returns the arguments for executeFrom.
     """
-    spec = find_spec(
-        f'.{module}', 
-        package=package
-        )    
+    spec = find_spec(f'.{module}', package=package)    
     sourceCode = spec.loader.get_source(
         f'{package}.{module}'
         )
-    entry.bind('<Return>', executionEntry)
-    print(
-        f"{module} {msgs['inspect']}\n", 
-        sourceCode + '\n', 
-        f"{msgs['proceed']}", 
-        sep='\n'
-        )
     
+    entry.bind('<Return>', executionEntry)
+    
+    log.write(f"{module} {msgs['inspect']}\n", 'green')
+    log.write(sourceCode, 'blue')
+    log.write(f"\n{msgs['proceed']}", 'green')
+
     return spec, sourceCode
         
 def executionEntry(event):
     """Control module execution upon user input."""
     userInput = event.widget.get()
-    event.widget.delete(0, tk.END)
-    print(userInput)
+    event.widget.delete(0, END)
+    log.write(userInput, 'green')
     
     if userInput == 'yes':
         executeFrom(*cache['handlers'])
     else:
         entry.bind('<Return>', moduleEntry)
-        print(
-            msgs['abort'],
-            msgs['nextop'],
-            *stratNames,
-            sep='\n'
-            )
+        for message in (msgs['abort'], msgs['nextop'], *pipelines):
+            log.write(message, 'green')
         
 def executeFrom(spec, sourceCode):
     """Execute the source code of a module."""
     print()
-    
     module = module_from_spec(spec)
-    codeObject = compile(
-        sourceCode, 
-        module.__spec__.origin, 
-        'exec'
-        )
-    exec(codeObject, module.__dict__)
-    
+    spec.loader.exec_module(module)
+
     entry.bind('<Return>', moduleEntry)
-    print(
-        f"\n{msgs['complete']}",
-        msgs['nextop'],
-        *stratNames,
-        sep='\n'
-        )
+    for message in (f"\n{msgs['complete']}", msgs['nextop'], *pipelines):
+        log.write(message, 'green')
 
 def setup():
     """Initialize the GUI window and divide it into two parts: the output and input boxes."""
-    root = tk.Tk()
+    
+    root = Tk()
     root.geometry("500x500")
     root.title("Replicant")
     
-    def write(self, string):
-        self.insert(tk.END, string)
-        self.see(tk.END)
-    
-    log = tk.Text(root)
-    log.insert(tk.END, msgs['welcome'] + '\n')
-    for strat in stratNames:
-        log.insert(tk.END, strat + '\n')
+    log = ScrolledText(root)
     log.configure(
         font='TkFixedFont', 
         bg='#000000', 
-        fg='#00FF00'
+        fg='#00FFFF'
         )
+    log.pack(fill=BOTH, expand=1)
+    log.tag_config('green', foreground="#00FF00")
+    log.tag_config('blue', foreground="#0000FF")
+    
+    def write(self, string, color=None):
+        if color is not None:
+            string += '\n'
+        log.insert(END, string, color)
+        log.see(END)
+    
     log.write = write.__get__(log)
-    log.pack(fill=tk.BOTH, expand=1)
-
-    entry = tk.Entry(root)
+    for text in (msgs['welcome'], *pipelines):
+        log.write(text, 'green')
+    
+    entry = Entry(root)
     entry.bind('<Return>', moduleEntry)
     entry.configure(
         font='TkFixedFont', 
@@ -135,20 +116,19 @@ def setup():
         fg='#00FF00',
         insertbackground='#00FF00'
         )
-    entry.icursor(tk.END)
-    entry.pack(fill=tk.BOTH, expand=1)
+    entry.icursor(END)
+    entry.pack(fill=BOTH, expand=1)
 
     return root, log, entry
 
 if __name__ == '__main__':  
     
-    path = f'./strats'
-    strats = [
-        f 
-        for f in listdir(path) 
-        if isfile(join(path, f))
+    path = './pipelines'
+    pipelines = [
+        splitext(file)[0]
+        for file in listdir(path)
+        if splitext(file)[1].startswith('.py')
         ]
-    stratNames = [s.split('.')[0] for s in strats]
     
     cache = {'handlers': ()}
 
@@ -164,5 +144,6 @@ if __name__ == '__main__':
 
     root, log, entry = setup()
         
-    with StandardOutput(log):
+    with StandardStream(log):
         root.mainloop()
+
